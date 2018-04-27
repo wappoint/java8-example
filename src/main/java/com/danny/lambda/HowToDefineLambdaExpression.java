53964a8bd591da45e1067db3e92c41ae6472c205
jiazhang <jiaz@yijale.com>
2018-04-27 18:12:11 +0800

    如何定义一个lambda表达式。

---
+++ src/main/java/com/danny/lambda/HowToDefineLambdaExpression.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.danny.lambda;
+
+/**
+ * @author 676002187@qq.com
+ * @date 2018-04-27
+ */
+public class HowToDefineLambdaExpression {
+
+	/**
+	 * @param args
+	 * @date 2018-04-27
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
