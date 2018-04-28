6f7dde6b48e2f405cd31d9750362ecd382edd0aa
jiazhang <jiaz@yijale.com>
2018-04-28 18:01:54 +0800

    如何定义lambda表达式。

--- src/main/java/com/danny/lambda/HowToDefineLambdaExpression.java
+++ src/main/java/com/danny/lambda/HowToDefineLambdaExpression.java
@@ -3,7 +3,24 @@
  */
 package com.danny.lambda;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import com.danny.domain.Apple;
+
 /**
+ * 描述： parameters list arrow lambda body </br>
+ * (o1,o2) -> o1.getColor().compareTo(o2.getColor());
+ * 
+ * </br>
+ * (parameters) -> expression. </br>
+ * (parameters) -> {statements;} </br>
+ * () -> {} </br>
+ * () -> ""
+ * 
  * @author 676002187@qq.com
  * @date 2018-04-27
  */
@@ -14,6 +31,22 @@ public class HowToDefineLambdaExpression {
 	 * @date 2018-04-27
 	 */
 	public static void main(String[] args) {
+		Comparator<Apple> compareByColor = new Comparator<Apple>() {
+
+			@Override
+			public int compare(Apple o1, Apple o2) {
+				return o1.getColor().compareTo(o2.getColor());
+			}
+		};
+		List<Apple> list = Collections.emptyList();
+		list.sort(compareByColor);
+
+		compareByColor = (o1, o2) -> o1.getColor().compareTo(o2.getColor());
+
+		Function<String, Integer> funLambda = s -> s.length();
+
+		Predicate<Apple> funApple = (Apple apple) -> apple.getColor()
+				.equals("green");
 
 	}
 
