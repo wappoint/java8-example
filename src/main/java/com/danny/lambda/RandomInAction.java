cd9dd135ae1201c04701b0a7dc088a5ac7713986
jiazhang <jiaz@yijale.com>
2018-05-02 17:53:49 +0800

    生成随机数。

---
+++ src/main/java/com/danny/lambda/RandomInAction.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.danny.lambda;
+
+import java.util.Random;
+import java.util.StringJoiner;
+
+/**
+ * @author 676002187@qq.com
+ * @date 2018-04-28
+ */
+public class RandomInAction {
+
+	/**
+	 * @param args
+	 * @date 2018-04-28
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			int result = generateRandom(5, 10);
+			System.out.println(result);
+		}
+		StringJoiner sj = new StringJoiner(",");
+		sj.add("apc").add("ddd").add("xxx");
+		System.out.println(sj);
+	}
+
+	private static int generateRandom(int min, int max) {
+		Random random = new Random();
+		return random.ints(min, max + 1).limit(1).findFirst().getAsInt();
+	}
+
+}
